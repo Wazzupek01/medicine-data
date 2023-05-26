9616f12c4c0271507b7988f3c9efa6f920bd66f2
Wazzupek01 <kpedrycz01@gmail.com>
2023-05-26 13:18:48 +0200

    initial commit

---
+++ src/main/java/com/pedryczpietrak/medicinedata/MedicineDataApplication.java
@@ -0,0 +1,13 @@
+package com.pedryczpietrak.medicinedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedryczpietrak/medicinedata/MedicineDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedryczpietrak.medicinedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
