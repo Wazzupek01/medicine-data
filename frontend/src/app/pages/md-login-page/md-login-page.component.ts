77b28cd29ed6fe5099ec059996dfdd8920684823
kamilpietrak <kamilpietrak123@gmail.com>
2023-05-30 21:42:16 +0200

    Added register page and fixed formGroup building into not deprecated function

--- frontend/src/app/pages/md-login-page/md-login-page.component.ts
+++ frontend/src/app/pages/md-login-page/md-login-page.component.ts
@@ -1,37 +1,43 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-md-login-page',
-  templateUrl: './md-login-page.component.html',
-  styleUrls: ['./md-login-page.component.css']
+    selector: 'app-md-login-page',
+    templateUrl: './md-login-page.component.html',
+    styleUrls: ['./md-login-page.component.css']
 })
-export class MdLoginPageComponent implements OnInit{
+export class MdLoginPageComponent implements OnInit {
 
-  protected loginError: boolean = false;
-  protected loginForm: FormGroup;
+    protected loginError: boolean = false;
+    protected loginForm: FormGroup;
 
-  constructor(
-    formBuilder: FormBuilder,
-    private router: Router,
-  ) {
-    this.loginForm = formBuilder.group({
-      email: new FormControl("", [
-        Validators.email,
-        Validators.required
-      ]),
-      password: new FormControl("", [
-        Validators.required,
-        Validators.minLength(8)
-      ])
-    });
-  }
+    constructor(
+        formBuilder: FormBuilder,
+        private router: Router,
+    ) {
+        this.loginForm = formBuilder.group({
+            email: formBuilder.control({
+                value: "",
+                validatorOrOpts: [
+                    Validators.required,
+                    Validators.email
+                ]
+            }),
+            password: formBuilder.control({
+                value: "",
+                validatorOrOpts: [
+                    Validators.required,
+                    Validators.minLength(8)
+                ]
+            }),
+        });
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  protected loginHandler() {
+    protected loginHandler() {
 
-  }
+    }
 }
--- frontend/src/app/pages/md-register-page/md-register-page.component.ts
+++ frontend/src/app/pages/md-register-page/md-register-page.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {
+    AbstractControl,
+    FormBuilder,
+    FormGroup,
+    ValidationErrors,
+    Validators
+} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-md-register-page',
-  templateUrl: './md-register-page.component.html',
-  styleUrls: ['./md-register-page.component.css']
+    selector: 'app-md-register-page',
+    templateUrl: './md-register-page.component.html',
+    styleUrls: ['./md-register-page.component.css']
 })
-export class MdRegisterPageComponent {
+export class MdRegisterPageComponent implements OnInit, OnDestroy {
 
+    protected registerForm: FormGroup;
+
+    constructor(
+        formBuilder: FormBuilder,
+        private router: Router,
+    ) {
+        this.registerForm = formBuilder.group(
+            {
+                email: formBuilder.control({
+                    value: "",
+                    validatorOrOpts: [
+                        Validators.required,
+                        Validators.email
+                    ]
+                }),
+                password: formBuilder.control({
+                    value: "",
+                    validatorOrOpts: [
+                        Validators.required,
+                        Validators.minLength(8)
+                    ]
+                }),
+                repeatPassword: formBuilder.control({
+                    value: "",
+                    validatorOrOpts: [
+                        Validators.required,
+                        Validators.minLength(8)
+                    ]
+                }),
+                role: formBuilder.control({
+                    value: "USER",
+                    validatorsOrOpts: []
+                }),
+            },
+            {
+                validators: [this.repeatPasswordValidator],
+                asyncValidators: [],
+                updateOn: "change"
+            }
+        );
+    }
+
+    ngOnInit() {
+    }
+
+    ngOnDestroy() {
+    }
+
+    protected registerUser() {
+
+    }
+
+    private repeatPasswordValidator = (control: AbstractControl): ValidationErrors | null => {
+        const password = control.get("password");
+        const repeatPassword = control.get("repeatPassword");
+        if (password != null && repeatPassword != null) {
+            if (password.value != repeatPassword.value) {
+                repeatPassword.setErrors({incorrect: true});
+                return {incorrect: {value: control.value}};
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
 }
