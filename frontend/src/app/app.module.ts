a70e54479b6add89c6b32955134d9cef4b344d6b
kamilpietrak <kamilpietrak123@gmail.com>
2023-05-30 19:25:43 +0200

    Added additional packages

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MdLoginPageComponent } from './pages/md-login-page/md-login-page.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import { MdRegisterPageComponent } from './pages/md-register-page/md-register-page.component';
+import {AgGridModule} from "ag-grid-angular";
+import {NgChartsModule} from "ng2-charts";
 
 @NgModule({
   declarations: [
     AppComponent,
-    MdLoginPageComponent
+    MdLoginPageComponent,
+    MdRegisterPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AgGridModule,
+    NgChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/pages/md-register-page/md-register-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-md-register-page',
+  templateUrl: './md-register-page.component.html',
+  styleUrls: ['./md-register-page.component.css']
+})
+export class MdRegisterPageComponent {
+
+}
